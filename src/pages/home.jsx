77af64ac9ffcd44e0cacf24b0219a22b466cb6f8
’Marina <148355147+MaRos10@users.noreply.github.com>
2024-04-23 08:41:56 +0200

    Edited homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <p className="homeText">
-        Hej och välkommen till SkillTrack! <br />
-        Detta är en sida som ska hjälpa dig "tracka" och utveckla dina kunskaper
-        inom Frontend-utveckling
-      </p>
+      <main className="homeWrapper">
+        <h1>Home</h1>
+        <p className="homeText">
+          Hej och välkommen till SkillTrack! <br />
+          Detta är en sida som ska hjälpa dig hålla koll på och utveckla dina
+          kunskaper inom Frontend-utveckling
+        </p>
+      </main>
     </>
   );
 }
